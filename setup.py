c00b6e7bef54e560bb10529cc5386c7f8bbe1678
Ryan Lim <rzlim08@gmail.com>
2020-01-31 11:39:47 -0500

    changed setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="MinimalPreprocessing",
     version="0.0.1",
     packages=["minimal_preprocessing"],
-    entry_points={"console_scripts": ["preprocess_peer = minimal_preprocessing.run_minimal_preprocessing:main"]},
+    entry_points={"console_scripts": ["preprocess = minimal_preprocessing.run_minimal_preprocessing:main"]},
 )
